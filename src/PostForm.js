4065c4dd950430b888341f3fa90e7b321665d036
Petherson Moreno <pethermoreno@gmail.com>
2018-11-13 11:44:47 -0200

    Applied basic styles

--- src/PostForm.js
+++ src/PostForm.js
@@ -20,9 +20,9 @@ class PostForm extends Component {
   };
   render() {
     return (
-      <div>
-        <h1>Create Post</h1>
-        <form onSubmit={this.handleSubmit}>
+      <div className="post-container">
+        <h1 className="post_heading">Create Post</h1>
+        <form className="form" onSubmit={this.handleSubmit}>
           <input
             required
             type="text"
