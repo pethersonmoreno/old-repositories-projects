f75df36d2a827f3b1fa6d562709087cce8a5b3d0
Petherson Moreno <pethermoreno@gmail.com>
2018-11-15 00:01:41 -0200

    Remove unused promises and Added usage of Firebase to store the shipLists

--- api/productType.js
+++ api/productType.js
@@ -3,48 +3,25 @@ import { mapObjectToList } from "./utils";
 const databaseProductTypes = database.ref("/productTypes");
 
 export const remove = id =>
-  new Promise((resolve, reject) => {
-    databaseProductTypes
-      .child(id)
-      .remove()
-      .then(() => {
-        resolve(id);
-      })
-      .catch(error => {
-        reject(error);
-      });
-  });
-export const edit = (id, updates) =>
-  new Promise((resolve, reject) => {
-    const { id: idField, ...otherFields } = updates;
-    databaseProductTypes
-      .child(id)
-      .set({
-        ...otherFields
-      })
-      .then(() => {
-        resolve({ id, updates: otherFields });
-      })
-      .catch(error => {
-        reject(error);
-      });
-  });
+  databaseProductTypes
+    .child(id)
+    .remove()
+    .then(() => id);
+export const edit = (id, { id: idField, ...otherFields }) =>
+  databaseProductTypes
+    .child(id)
+    .set({
+      ...otherFields
+    })
+    .then(() => ({ id, updates: otherFields }));
 export const add = productType =>
-  new Promise((resolve, reject) => {
-    const id = databaseProductTypes.push().key;
-    edit(id, productType)
-      .then(({ id, updates }) => resolve({ ...updates, id }))
-      .catch(error => reject(error));
-  });
+  edit(databaseProductTypes.push().key, productType).then(
+    ({ id, updates }) => ({ ...updates, id })
+  );
 export const getAll = () =>
-  new Promise((resolve, reject) => {
-    databaseProductTypes
-      .once("value")
-      .then(snapshot => {
-        resolve(mapObjectToList(snapshot.val(), "id"));
-      })
-      .catch(error => reject(error));
-  });
+  databaseProductTypes
+    .once("value")
+    .then(snapshot => mapObjectToList(snapshot.val(), "id"));
 export const listenChanges = listenCallBack => {
   databaseProductTypes.on("value", snapshot => {
     const productTypes = mapObjectToList(snapshot.val(), "id");
