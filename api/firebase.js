fc9beeef2e8b19cf9814516eb636f7c76b3f7111
Petherson Moreno <pethermoreno@gmail.com>
2018-11-21 20:44:10 -0200

    Added config firebase variables to .env file

--- api/firebase.js
+++ api/firebase.js
@@ -2,10 +2,10 @@ import * as firebase from "firebase/app";
 import "firebase/database";
 import "firebase/auth";
 var config = {
-  apiKey: "AIzaSyCWHXnAGCLct8vexeg6guAPMoE5scFfc_0",
-  authDomain: "nao-esqueca.firebaseapp.com",
-  databaseURL: "https://nao-esqueca.firebaseio.com",
-  storageBucket: "nao-esqueca.appspot.com"
+  apiKey: process.env.APP_FIREBASE_API_KEY,
+  authDomain: process.env.APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.APP_FIREBASE_DATABASE_URL,
+  storageBucket: process.env.APP_FIREBASE_STORAGE_BUCKET
 };
 const defaultFirebase = firebase.initializeApp(config);
 export const database = defaultFirebase.database();
