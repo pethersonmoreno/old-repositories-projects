31e228244f8a85a40230eb985c71f7790e7d7b2e
Petherson Moreno <pethermoreno@gmail.com>
2018-11-25 08:39:57 -0200

    Applied story 'Como um usuário ao recarregar a página eu quero permanecer na mesma página se o meu status de autorização não foi modificado'

--- store.js
+++ store.js
@@ -1,14 +1,27 @@
 import { createStore, applyMiddleware, combineReducers } from "redux";
 import thunkMiddleware from "redux-thunk";
 import promiseMiddleware from "redux-promise-middleware";
-import logger from "./middlewares/logger";
 import * as reducers from "./ducks";
+import { loadState, saveState } from "./localStorage";
+import logger from "./middlewares/logger";
 
 const rootReducer = combineReducers(reducers);
+
+const persistedState = loadState();
 
 let middlewares = [promiseMiddleware(), thunkMiddleware];
 if (process.env.NODE_ENV === "development") {
   middlewares.push(logger);
 }
 
-export default createStore(rootReducer, applyMiddleware(...middlewares));
+const store = createStore(
+  rootReducer,
+  persistedState,
+  applyMiddleware(...middlewares)
+);
+
+store.subscribe(() => {
+  saveState(store.getState());
+});
+
+export default store;
