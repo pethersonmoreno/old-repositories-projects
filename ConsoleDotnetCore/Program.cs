40924e8fd373b3df6f151db12f80f02a089193e8
Petherson Moreno <pethermoreno@gmail.com>
2019-02-09 21:04:18 -0200

    Created DotnetCore Console Version

---
+++ ConsoleDotnetCore/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Threading.Tasks;
+using WatcherExample.DirectoryWatcher;
+
+namespace WatcherExample.ConsoleDotnetCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Run();
+            Console.WriteLine("Press 'q' to quit the sample.");
+            while (Console.ReadKey().KeyChar != 'q') ;
+            Environment.Exit(0);
+        }
+
+        private static void Run()
+        {
+            Task.Factory.StartNew(() =>
+            {
+                string path = @"C:\\Users\\DevPlace Developer\\Desktop\\TesteWatcher";
+                string filter = "*.pdf";
+                var cWatcher = new ContainerWatcher(path, filter);
+                cWatcher.NewFileCreated += CWatcher_NewFileCreated;
+                cWatcher.WatcherWillStartAgain += CWatcher_WatcherWillStartAgain; ;
+                cWatcher.StartWatching();
+            });
+        }
+
+        private static int countFile = 0;
+        private static void CWatcher_NewFileCreated(string filePath)
+        {
+            Console.WriteLine("### " + (++countFile) + " - Arquivo Criado: " + filePath);
+        }
+
+        private static void CWatcher_WatcherWillStartAgain()
+        {
+            Console.WriteLine("### - Watcher vai iniciar novamente");
+        }
+    }
+}
