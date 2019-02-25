88bb797df57b590c5e330878b4330dc546ede8e0
Petherson Moreno <pethermoreno@gmail.com>
2019-02-25 00:27:12 -0300

    Configured PHPUnitStandard to use in projects

---
+++ config-phpcs/.phpcs/PHPUnitStandard/Sniffs/Testing/ExtraneousClassSniff.php
@@ -0,0 +1,48 @@
+<?php
+
+class PHPUnitStandard_Sniffs_Testing_ExtraneousClassSniff
+implements PHP_CodeSniffer_Sniff {
+
+    /**
+     * Returns the token types that this sniff is interested in.
+     *
+     * @return array(int)
+     */
+    public function register() {
+        return array(
+            T_CLASS,
+        );
+    }
+
+    /**
+     * Processes the tokens that this sniff is interested in.
+     *
+     * @param PHP_CodeSniffer_File $phpcsFile The file where the token was found.
+     * @param int                  $stackPtr  The position in the stack where
+     *                                        the token was found.
+     *
+     * @return void
+     */
+    public function process(PHP_CodeSniffer_File $phpcsFile, $stackPtr) {
+        $filename = $phpcsFile->getFileName();
+        if(!preg_match("@.*/tests/phpunit/(.*).php@", $filename, $matches)) {
+            $phpcsFile->addWarning(
+                'Filepath does not match expected convention',
+                $stackPtr
+            );
+            return;
+        }
+        $expected = str_replace("/", "_", $matches[1]);
+
+        $declarationName = $phpcsFile->getDeclarationName($stackPtr);
+        if ($declarationName != $expected) {
+            $phpcsFile->addError(
+                 'Unexpected extraneous class found: %s.  Expected: %s',
+                 $stackPtr,
+                 'Found',
+                  array($declarationName, $expected)
+            );
+        }
+    }
+}
+
