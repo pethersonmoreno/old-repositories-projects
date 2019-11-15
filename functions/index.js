1605b6a9ebd57f576a6841374b01aee66983ceda
Petherson Moreno <pethermoreno@gmail.com>
2019-11-15 17:57:25 -0300

    refactor: changed function start to only set process env if exist configs to use

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,9 @@
 const functions = require('firebase-functions');
 
-process.env.FIREBASE_API_KEY = functions.config().cashflowapi.firebase.apikey;
-process.env.FIREBASE_PROJECT_ID = functions.config().cashflowapi.firebase.projectid;
+if (functions.config().cashflowapi && functions.config().cashflowapi.firebase) {
+  process.env.FIREBASE_API_KEY = functions.config().cashflowapi.firebase.apikey;
+  process.env.FIREBASE_PROJECT_ID = functions.config().cashflowapi.firebase.projectid;
+}
 const server = require('cash-flow-api');
 
 exports.api = functions.https.onRequest(server._events.request);
