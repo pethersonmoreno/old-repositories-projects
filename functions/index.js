b4f707291ff107e67aedf00f22b1f332b2c15e3e
Petherson Moreno <pethermoreno@gmail.com>
2019-11-15 17:35:41 -0300

    chore: started config

---
+++ functions/index.js
@@ -0,0 +1,7 @@
+const functions = require('firebase-functions');
+
+process.env.FIREBASE_API_KEY = functions.config().cashflowapi.firebase.apikey;
+process.env.FIREBASE_PROJECT_ID = functions.config().cashflowapi.firebase.projectid;
+const server = require('cash-flow-api');
+
+exports.api = functions.https.onRequest(server._events.request);
