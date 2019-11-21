04d781e326b27928a9e7314d930647ae591cafbb
Petherson Moreno <pethermoreno@gmail.com>
2019-11-20 23:31:02 -0300

    feat: added a simple list of people

--- src/app/modules/People/PeopleList.js
+++ src/app/modules/People/PeopleList.js
@@ -1,5 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import * as firebase from 'firebase/app';
+import peopleApi from '../../../api/people';
 
-const PeopleList = () => <div>People 2</div>;
+const PeopleList = () => {
+  const [list, setList] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      const token = await firebase.auth().currentUser.getIdToken();
+      const listLoaded = await peopleApi.getList(token);
+      setList(listLoaded);
+    };
+    fetchData();
+  }, []);
+  return (
+    <div>
+      <ul>
+        {list.map(person => (
+          <li key={person.id}>{person.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 export default PeopleList;
