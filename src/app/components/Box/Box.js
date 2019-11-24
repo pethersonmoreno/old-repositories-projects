d86dbf7c3da1c973277e83dbd61ce9d8d77ef017
Petherson Moreno <pethermoreno@gmail.com>
2019-11-24 20:50:46 -0300

    fix: updated Sign page

---
+++ src/app/components/Box/Box.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import classnames from 'classnames';
+
+const Box = ({
+  children, center, fill, flex
+}) => (
+  <div className={classnames('box', {
+    'box-center': center,
+    'box-fill': fill,
+    'box-flex': flex,
+  })}
+  >
+    <div className="box_container">
+      {children}
+    </div>
+  </div>
+);
+Box.propTypes = {
+  children: PropTypes.element.isRequired,
+  center: PropTypes.bool,
+  fill: PropTypes.bool,
+  flex: PropTypes.bool,
+};
+Box.defaultProps = {
+  center: false,
+  fill: false,
+  flex: false,
+};
+
+export default Box;
--- src/app/routers/MainRouter.js
+++ src/app/routers/MainRouter.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {
   Switch, Route, Redirect
 } from 'react-router-dom';
-import SigIn from '../auth/SigIn/SigIn';
+import SignIn from '../auth/SigIn/SignIn';
 import AppAuthenticated from '../AppAuthenticated';
 import NotAuthorized from '../auth/NotAuthorized';
 
@@ -10,7 +10,7 @@ const MainRouter = () => (
   <Switch>
     <Route path="/" exact render={() => <Redirect to="/signIn" />} />
     <Route exact path="/notAuthorized" component={NotAuthorized} />
-    <Route exact path="/signIn" component={SigIn} />
+    <Route exact path="/signIn" component={SignIn} />
     <Route component={AppAuthenticated} />
   </Switch>
 );
