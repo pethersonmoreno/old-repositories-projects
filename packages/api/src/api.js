340a16eb409bc7510cebd9822f394f9a92252a3f
Petherson Moreno <pethermoreno@gmail.com>
2019-12-07 05:21:35 -0300

    feat: created /validateTask endpoint in API

--- packages/api/src/api.js
+++ packages/api/src/api.js
@@ -1,12 +1,30 @@
 const Koa = require("koa");
+const bodyParser = require('koa-bodyparser');
+const router = require('koa-route');
+const validateTask = require('validator-task');
+
 const app = (module.exports = new Koa());
 
-app.use(async function(ctx) {
-  ctx.body = "Hello World";
-});
+app.use(bodyParser());
+
+app.use(router.get('/', async function (ctx) {
+  ctx.body = "API Working";
+}));
+
+app.use(router.post('/validateTask', async function (ctx) {
+  const task = ctx.request.body;
+  try {
+    await validateTask(task);
+    ctx.status = 200;
+    ctx.body = "Task OK";
+  } catch (error) {
+    ctx.status = 400;
+    ctx.body = "Invalid Task";
+  }
+}));
 
 if (!module.parent) {
-  const port = 3000;
+  const port = 3001;
   app.listen(port);
   console.log("Listening in http://0.0.0.0:" + port);
 }
