3156c888656ed7090912e7e98c03db4b9a752cca
Petherson Moreno <pethermoreno@gmail.com>
2020-01-03 23:13:56 -0300

    feat: added flows report by month

--- src/modules/app/CashFlows/components/CashFlowsReport/index.js
+++ src/modules/app/CashFlows/components/CashFlowsReport/index.js
@@ -1,34 +1,126 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, { useState } from 'react';
+import ReactDatePicker from 'react-datepicker';
 import moment from 'moment';
+import {
+  Paper, DataTable, TableHeader, TableRow, TableColumn, TableBody
+} from 'react-md';
 import useCashFlowsList from '../../../../utils/hooks/useCashFlowsList';
-import CashFlowsListView from './CashFlowsListView';
+import useCashFlowDescriptionsList from '../../../../utils/hooks/useCashFlowDescriptionsList';
+import formatMoneyValue from '../../../../utils/helpers/formatMoneyValue';
+import './CashFlowsReport.scss';
 
-const orderList = list => list.sort((flowA, flowB) => flowB.dateTime - flowA.dateTime);
+const listIgnoreDescriptions = [
+  '5WGg1E8ES5wG8qj7obDW', // Gastos Cartão de Crédito
+  'c6Agf4mYQrbPFT2YLBeg', // Pagamento Fatura
+  'Iv9iIfXebD6rdmTwtYv2', // Transferência
+  'GIgJXgw2MS0044HgiFlr', // Saque
+];
 
-const groupByDates = list => list.reduce((newList, cashFlow) => {
-  let group = null;
-  if (newList.length) {
-    const lastGroup = newList[newList.length - 1];
-    if (moment(lastGroup.date).format('YYYY-MM-DD') === moment(cashFlow.dateTime).format('YYYY-MM-DD')) {
-      group = lastGroup;
+const getCashFlowDescription = (cashFlowDescriptionsList, cashFlowDescriptionId) => {
+  const cashFlowDescription = cashFlowDescriptionsList.find(
+    cfd => cfd.id === cashFlowDescriptionId
+  );
+  if (cashFlowDescription) {
+    return cashFlowDescription.name;
+  }
+  return '(Not found)';
+};
+
+const orderListValue = (list, isDesc) =>
+  list.sort((flowA, flowB) => (flowA.value - flowB.value) * (isDesc ? -1 : 1));
+
+const groupByDescription = list => list
+  .sort((flowA, flowB) => flowB.cashFlowDescriptionId - flowA.cashFlowDescriptionId)
+  .reduce((newList, cashFlow) => {
+    let group = newList.find(f => f.cashFlowDescriptionId === cashFlow.cashFlowDescriptionId);
+    if (!group) {
+      group = { cashFlowDescriptionId: cashFlow.cashFlowDescriptionId, value: 0, items: [] };
+      newList.push(group);
     }
-  }
-  if (group === null) {
-    group = { date: cashFlow.dateTime, items: [] };
-    newList.push(group);
-  }
-  group.items.push(cashFlow);
-  return newList;
-}, []);
+    group.items.push(cashFlow);
+    group.value += cashFlow.value * (cashFlow.inOut ? -1 : 1);
+    return newList;
+  }, []);
 
-const CashFlowsReport = () =>  {
+const CashFlowsReport = () => {
+  const [monthDate, setMonthDate] = useState(new Date());
+  const [cashFlowDescriptionsList] = useCashFlowDescriptionsList();
   const [list] = useCashFlowsList();
-  const orderedList = orderList(list);
-  const grouped = groupByDates(orderedList);
+  const monthDateString = moment(monthDate).format('YYYY-MM');
+  const listFiltered = list
+    .filter(cashFlow => !listIgnoreDescriptions.find(id => cashFlow.cashFlowDescriptionId === id))
+    .filter(cashFlow => moment(cashFlow.dateTime).format('YYYY-MM') === monthDateString);
+  const groupedList = groupByDescription(listFiltered);
+  const negativeDescriptions = orderListValue(groupedList.filter(group => group.value < 0), false);
+  const positiveDescriptions = orderListValue(groupedList.filter(group => group.value >= 0), true);
   return (
-    <div>
-    </div>
+    <Paper>
+      <div>
+        Month:
+        {' '}
+        <ReactDatePicker
+          selected={monthDate}
+          onChange={date => setMonthDate(date)}
+          dateFormat="yyyy-MM"
+          showMonthYearPicker
+        />
+      </div>
+      <h2>Negative Descriptions</h2>
+      <DataTable plain>
+        <TableHeader>
+          <TableRow>
+            <TableColumn>Description</TableColumn>
+            <TableColumn>Value</TableColumn>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {negativeDescriptions.map(group => (
+            <TableRow key={group.cashFlowDescriptionId}>
+              <TableColumn>
+                {getCashFlowDescription(cashFlowDescriptionsList, group.cashFlowDescriptionId)}
+              </TableColumn>
+              <TableColumn>{formatMoneyValue(group.value)}</TableColumn>
+            </TableRow>
+          ))}
+        </TableBody>
+      </DataTable>
+      <h2>Positive Descriptions</h2>
+      <DataTable plain>
+        <TableHeader>
+          <TableRow>
+            <TableColumn>Description</TableColumn>
+            <TableColumn>Value</TableColumn>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {positiveDescriptions.map(group => (
+            <TableRow key={group.cashFlowDescriptionId}>
+              <TableColumn>
+                {getCashFlowDescription(cashFlowDescriptionsList, group.cashFlowDescriptionId)}
+              </TableColumn>
+              <TableColumn>{formatMoneyValue(group.value)}</TableColumn>
+            </TableRow>
+          ))}
+        </TableBody>
+      </DataTable>
+      <h2>Total</h2>
+      <DataTable plain>
+        <TableBody>
+          <TableRow>
+            <TableColumn>Negative</TableColumn>
+            <TableColumn>
+              {formatMoneyValue(negativeDescriptions.reduce((sum, group) => sum + group.value, 0))}
+            </TableColumn>
+          </TableRow>
+          <TableRow>
+            <TableColumn>Positive</TableColumn>
+            <TableColumn>
+              {formatMoneyValue(positiveDescriptions.reduce((sum, group) => sum + group.value, 0))}
+            </TableColumn>
+          </TableRow>
+        </TableBody>
+      </DataTable>
+    </Paper>
   );
 };
 
