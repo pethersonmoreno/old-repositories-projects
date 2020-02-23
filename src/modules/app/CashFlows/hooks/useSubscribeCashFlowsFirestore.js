094ed7ec50ef8bd04ae504c8f33adbef4465f3de
Petherson Moreno <pethermoreno@gmail.com>
2020-02-23 07:47:40 -0300

    feat: got more performance requesting only cash flows of month

--- src/modules/app/CashFlows/hooks/useSubscribeCashFlowsFirestore.js
+++ src/modules/app/CashFlows/hooks/useSubscribeCashFlowsFirestore.js
@@ -1,21 +1,31 @@
 import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
-import subscribeFirestore from '../../../utils/firestore/subscribeFirestore';
+import moment from 'moment';
+import { firestore } from '../../../utils/api/firebase';
+import mapDocumentToData from '../../../utils/api/firebaseUtil/mapDocumentToData';
 import { COLLECTION_NAME } from '../constants';
 import * as actions from '../actions/actionsCashFlows';
+import { useCashFlowsCurrentMonth } from '../selectors/selectorsCashFlows';
 
 const useSubscribePeopleFirestore = () => {
   const dispatch = useDispatch();
+  const currentMonth = useCashFlowsCurrentMonth();
+  const monthDateStart = moment(`${currentMonth}-01T00:00:00`);
+  const monthDateEndLimit = moment(monthDateStart).add('month', 1);
   useEffect(() => {
-    const unsubscribeFirestore = subscribeFirestore(
-      COLLECTION_NAME,
-      list => dispatch(actions.getCashFlows(list.map(item => ({
-        ...item,
-        dateTime: item.dateTime.toString(),
-      })))),
-    );
+    const unsubscribeFirestore = firestore
+      .collection(COLLECTION_NAME)
+      .where('dateTime', '>=', monthDateStart.toDate())
+      .where('dateTime', '<', monthDateEndLimit.toDate())
+      .onSnapshot(querySnapshot => {
+        const newList = querySnapshot.docs.map(mapDocumentToData);
+        dispatch(actions.getCashFlows(newList.map(item => ({
+          ...item,
+          dateTime: item.dateTime.toString(),
+        }))));
+      });
     return unsubscribeFirestore;
-  }, [dispatch]);
+  }, [dispatch, monthDateEndLimit, monthDateStart]);
 };
 
 export default useSubscribePeopleFirestore;
--- src/modules/app/CashFlows/selectors/selectorsCashFlows.js
+++ src/modules/app/CashFlows/selectors/selectorsCashFlows.js
@@ -1,10 +1,7 @@
 import { useSelector } from 'react-redux';
-import moment from 'moment';
 
 export const useCashFlowsCurrentMonth = () => useSelector(state => state.cashFlows.currentMonth);
 export const useCashFlow = id => useSelector(state =>
   state.cashFlows.list.find(item => item.id === id));
-export const useCashFlowListMonth = month =>
-  useSelector(state => state.cashFlows.list
-    .filter(cashFlow =>
-      moment(new Date(cashFlow.dateTime)).format('YYYY-MM') === month));
+export const useCashFlowListMonth = () =>
+  useSelector(state => state.cashFlows.list);
