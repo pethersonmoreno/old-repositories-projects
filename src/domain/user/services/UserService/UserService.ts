c6269d543a3a06b29a3ad2989c43a9ed23a6b2fe
Petherson Moreno <pethermoreno@gmail.com>
2021-06-27 15:51:04 -0300

    feat: added update to UserService

--- src/domain/user/services/UserService/UserService.ts
+++ src/domain/user/services/UserService/UserService.ts
@@ -1,12 +1,14 @@
 import { UserRepository } from "../../repositories";
-import { Email, Nickname, Password, Role } from "../../valueObjects";
+import { Email, IdentityUuid, Nickname, Password, Role } from "../../valueObjects";
 import { User } from "../../entities";
 
-export type NewUserDTO = {
+export type UpdateUserDTO = {
+    userId: string;
     email: string;
     nickname: string;
     password: string;
 };
+export type NewUserDTO = Omit<UpdateUserDTO, 'userId'>;
 
 export default class UserService {
     constructor(
@@ -35,5 +37,29 @@ export default class UserService {
             role
         });
         await this.userRepository.save(newUser);
+    }
+
+    async update(updateUserDto: UpdateUserDTO): Promise<void> {
+        const userId = IdentityUuid.createFromUuid(updateUserDto.userId);
+        const user = await this.userRepository.findByUserId(userId)
+        if(!user){
+            throw new Error(`User not found with ID ${updateUserDto.userId}`);
+        }
+        const newEmail = Email.create(updateUserDto.email)
+        if(await this.existOtherUserByEmail(user, newEmail)){
+            throw new Error(`Already exists another user with email ${newEmail.value}`);
+        }
+        user.updateEmail(newEmail.value);
+        user.updateNickname(updateUserDto.nickname);
+        user.updatePassword(updateUserDto.password);
+        await this.userRepository.save(user);
+    }
+
+    private async existOtherUserByEmail(user: User, newEmail: Email): Promise<boolean>{
+        if(newEmail.equals(user.email)){
+            return false;
+        }
+        const otherByEmail = await this.userRepository.findByEmail(newEmail);
+        return !!otherByEmail;
     }
 }
