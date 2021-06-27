6d48f2205334ed5bd6a40e1622d0e57e2f8e070e
Petherson Moreno <pethermoreno@gmail.com>
2021-06-27 15:18:07 -0300

    feat: added register to UserService

--- src/domain/user/services/UserService/UserService.ts
+++ src/domain/user/services/UserService/UserService.ts
@@ -1,5 +1,12 @@
 import { UserRepository } from "../../repositories";
-import { Email } from "../../valueObjects";
+import { Email, Nickname, Password, Role } from "../../valueObjects";
+import { User } from "../../entities";
+
+export type NewUserDTO = {
+    email: string;
+    nickname: string;
+    password: string;
+};
 
 export default class UserService {
     constructor(
@@ -10,5 +17,23 @@ export default class UserService {
         const emailObj = Email.create(email);
         const userByEmail = await this.userRepository.findByEmail(emailObj);
         return userByEmail.password.matchRawPassword(password);
+    }
+
+    async register(newUserDto: NewUserDTO): Promise<void> {
+        const email = Email.create(newUserDto.email);
+        const nickname = Nickname.create(newUserDto.nickname);
+        const password = Password.create(newUserDto.password);
+        const role = Role.createPokemonTrainer();
+        const userByEmail = await this.userRepository.findByEmail(email);
+        if(userByEmail){
+            throw new Error(`Already exists another user with email ${email.value}`);
+        }
+        const newUser = User.create({
+            email,
+            nickname,
+            password,
+            role
+        });
+        await this.userRepository.save(newUser);
     }
 }
