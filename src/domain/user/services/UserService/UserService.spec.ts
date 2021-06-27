4abdeaf837aecc24e496105bf5b3aa465cf61450
Petherson Moreno <pethermoreno@gmail.com>
2021-06-27 15:02:28 -0300

    test: updated MemoryUserRepository to only change inside it

--- src/domain/user/services/UserService/UserService.spec.ts
+++ src/domain/user/services/UserService/UserService.spec.ts
@@ -10,22 +10,34 @@ class MemoryUserRepository implements UserRepository {
         this.allUsers = [];
     }
     findByUserId(userId: IdentityUuid): Promise<User> {
-        return Promise.resolve(this.allUsers.find(user => user.userId.equals(userId)));
+        return Promise.resolve(this.cloneUserIfDefined(this.allUsers.find(user => user.userId.equals(userId))));
     }
     findByEmail(email: Email): Promise<User> {
-        return Promise.resolve(this.allUsers.find(user => user.email.equals(email)));
+        return Promise.resolve(this.cloneUserIfDefined(this.allUsers.find(user => user.email.equals(email))));
+    }
+    private cloneUserIfDefined(user?: User) {
+        if(!user){
+            return undefined;
+        }
+        return User.createWithUserId({
+            userId: user.userId,
+            email: user.email,
+            nickname: user.nickname,
+            password: user.password,
+            role: user.role,
+        })
     }
     async save(user: User): Promise<void> {
         const currentUser = await this.findByUserId(user.userId);
         if(currentUser){
             this.allUsers = this.allUsers.map(itemUser => {
                 if(itemUser.equals(user)){
-                    return user;
+                    return this.cloneUserIfDefined(user);
                 }
                 return itemUser;
             });
         } else {
-            this.allUsers = [...this.allUsers, user];
+            this.allUsers = [...this.allUsers, this.cloneUserIfDefined(user)];
         }
     }
 
@@ -51,6 +63,7 @@ describe('UserService', () => {
         });
         const userRepository = new MemoryUserRepository();
         userRepository.save(user1);
+        userRepository.save(user2);
         const userService = new UserService(userRepository);
 
         it('should be valid if email with password exists', () => {
