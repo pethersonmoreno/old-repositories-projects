3e8bd5977b625e2c50286baf26b37953f46dfa71
Petherson Moreno <pethermoreno@gmail.com>
2021-06-27 16:54:21 -0300

    feat: added exports to value objects

---
+++ src/domain/pokemon/valueObjects/index.ts
@@ -0,0 +1,3 @@
+export { default as NumberInteger } from './NumberInteger/NumberInteger';
+export { default as PokemonName } from './PokemonName/PokemonName';
+export { default as PokemonType } from './PokemonType/PokemonType';
