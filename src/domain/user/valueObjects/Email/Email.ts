c11bdeb857dd72db7e1bcfd89a19acdc6ceaa2cf
Petherson Moreno <pethermoreno@gmail.com>
2021-06-27 12:07:12 -0300

    feat: added email address validation

--- src/domain/user/valueObjects/Email/Email.ts
+++ src/domain/user/valueObjects/Email/Email.ts
@@ -1,4 +1,5 @@
 import { v4 as uuidv4 } from 'uuid';
+import isEmail from 'validator/lib/isEmail';
 
 const securityValue = uuidv4();
 
@@ -27,6 +28,18 @@ export default class Email {
     }
 
     public static create(value: string) {
+        const options = {
+            allow_display_name: false,
+            require_display_name: false,
+            allow_utf8_local_part: true,
+            require_tld: true,
+            ignore_max_length: false,
+            allow_ip_domain: false,
+            domain_specific_validation: true,
+        }
+        if(!isEmail(value, options)){
+            throw new Error("Invalid Email addresss");
+        }
         return new Email(value, securityValue);
     }
 }
