253f508f1a97fb340d8a43d8027e06a45d9ddf16
Petherson Moreno <pethermoreno@gmail.com>
2021-06-27 14:44:06 -0300

    feat: added validateLogin to UserService

---
+++ src/domain/user/services/UserService/UserService.ts
@@ -0,0 +1,14 @@
+import { UserRepository } from "../../repositories/UserRepository";
+import { Email } from "../../valueObjects";
+
+export default class UserService {
+    constructor(
+        private readonly userRepository: UserRepository,
+    ) {}
+
+    async validateLogin(email: string, password: string): Promise<boolean> {
+        const emailObj = Email.create(email);
+        const userByEmail = await this.userRepository.findByEmail(emailObj);
+        return userByEmail.password.matchRawPassword(password);
+    }
+}
