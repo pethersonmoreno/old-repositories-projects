2c1b58aa6fbb4874bf94ae8457f16e4a2696e585
Petherson Moreno <pethermoreno@gmail.com>
2021-06-27 16:32:01 -0300

    feat: created PokemonType value object

---
+++ src/domain/pokemon/valueObjects/PokemonType/PokemonType.ts
@@ -0,0 +1,60 @@
+import { v4 as uuidv4 } from 'uuid';
+
+const securityValue = uuidv4();
+
+type ValidPokemonType = 'Normal' | 'Fighting' | 'Flying' | 'Poison' | 'Ground' | 'Rock' | 'Bug' | 'Ghost' | 'Steel' | 'Fire' | 'Water' | 'Grass' | 'Electric' | 'Psychic' | 'Ice' | 'Dragon' | 'Fairy' | 'Dark';
+
+const listValidPokemonTypes = [
+  'Normal',
+  'Fighting',
+  'Flying',
+  'Poison',
+  'Ground',
+  'Rock',
+  'Bug',
+  'Ghost',
+  'Steel',
+  'Fire',
+  'Water',
+  'Grass',
+  'Electric',
+  'Psychic',
+  'Ice',
+  'Dragon',
+  'Fairy',
+  'Dark',
+]
+
+
+
+export default class PokemonType {
+  private constructor(value: ValidPokemonType, secValue: any) {
+    if (secValue !== securityValue) {
+      throw new Error('Invalid Instantiation');
+    }
+    (this as any)[securityValue] = {
+      value,
+    };
+  }
+
+  public get value(): ValidPokemonType {
+    return (this as any)[securityValue].value;
+  }
+
+  public equals(value: PokemonType): boolean {
+    if (value === null || value === undefined) {
+      return false;
+    }
+    if (!(value instanceof PokemonType)) {
+      return false;
+    }
+    return this.value === value.value;
+  }
+
+  public static create(value: ValidPokemonType) {
+    if(!listValidPokemonTypes.find(item => item === value)){
+      throw new Error('Invalid Pokemon Type');
+    }
+    return new PokemonType(value, securityValue);
+  }
+}
