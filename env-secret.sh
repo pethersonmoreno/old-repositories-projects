1244e3d7cb64806f239997df6c8b8ffe19910ea2
Petherson Moreno <pethermoreno@gmail.com>
2022-02-03 18:58:13 -0300

    refactor: removed unnecessary echo

--- env-secret.sh
+++ env-secret.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 scriptDir=$(dirname -- "$(readlink -f -- "$BASH_SOURCE")")
 pathGetEnvSecret=""
-echo "$scriptDir/get_env_secret"
 if [ -f "$scriptDir/get_env_secret" ]; then
     pathGetEnvSecret="$scriptDir/get_env_secret"
 else
