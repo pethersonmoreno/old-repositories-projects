8696d76d0f8d3df99be39af79ecb95998c20a3e1
Petherson Moreno <pethermoreno@gmail.com>
2022-05-08 16:28:35 -0300

    feat: updated page layout to remove button

--- prepare-page-to-read-card.js
+++ prepare-page-to-read-card.js
@@ -1,15 +1,24 @@
-const navigationClassNameList = document.querySelector("#ak-side-navigation").parentElement.className.split(" ");
-const navigationClassName = navigationClassNameList[navigationClassNameList.length - 1];
-const rightColumnClassNameList = document.querySelector("div[data-test-id='issue.views.issue-details.issue-layout.right-most-column']").parentElement.className.split(" ")
-const rightColumnClassName = rightColumnClassNameList[rightColumnClassNameList.length - 1];
-const headerClassName = document.querySelector("header").parentElement.className;
-const cssUpdates = document.createElement("style");
-cssUpdates.innerHTML = ""
-cssUpdates.innerHTML += "body { --leftSidebarWidth:0px; --topNavigationHeight: 0px; }"
-cssUpdates.innerHTML += "."+navigationClassName+" { with:0; overflow: hidden }"
-cssUpdates.innerHTML += "."+rightColumnClassNameList+" { with:0; overflow: hidden }"
-cssUpdates.innerHTML += "."+headerClassName+" { height:0; overflow: hidden }"
-cssUpdates.innerHTML += "."+rightColumnClassName+" { width:0; flex-grow:0 }"
-cssUpdates.innerHTML += ".ak-renderer-document { font-size:1.5em }"
-document.head.appendChild(cssUpdates);
-document.querySelector("[data-testid='ContextualNavigation-grab-area']").onmousemove = null;
+const preparePageToReadCard = (retries) => {
+    if (retries < 0) {
+        return;
+    }
+    const navigationClassNameList = document.querySelector("#ak-side-navigation").parentElement.className.split(" ");
+    const navigationClassName = navigationClassNameList[navigationClassNameList.length - 1];
+    const rightColumnClassNameList = document.querySelector("#jira-issue-header-actions").parentElement.parentElement.className.split(" ")
+    const rightColumnClassName = rightColumnClassNameList[rightColumnClassNameList.length - 1];
+    const headerClassName = document.querySelector("header").parentElement.className;
+    const cssUpdates = document.createElement("style");
+    cssUpdates.innerHTML = ""
+    cssUpdates.innerHTML += "body { --leftSidebarWidth:0px; --topNavigationHeight: 0px; }"
+    cssUpdates.innerHTML += "."+navigationClassName+" { with:0; overflow: hidden; position: absolute; left:-200px;top:0px; }"
+    cssUpdates.innerHTML += "."+navigationClassName+" > div > div button[data-resize-button]  { opacity: 0; display: none }"
+    cssUpdates.innerHTML += "."+headerClassName+" { height:0; overflow: hidden }"
+    cssUpdates.innerHTML += "."+rightColumnClassName+" { width:0; flex-grow:0 }"
+    cssUpdates.innerHTML += ".ak-renderer-document { font-size:1.5em }"
+    document.head.appendChild(cssUpdates);
+    document.querySelector("#ak-side-navigation > div > div > button").setAttribute ("onmousemove", null);
+    setTimeout(()=>{
+        preparePageToReadCard(retries-1);
+    }, 500);
+};
+preparePageToReadCard(3);
